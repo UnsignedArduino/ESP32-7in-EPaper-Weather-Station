f96ae6baad6efd25c53da4b65928577dbe93eb36
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-06-08 22:35:07 -0400

    Add basic wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,44 @@
 #include <Arduino.h>
+#include <WiFiManager.h>
+
+int8_t connectToWiFi() {
+  Serial.println("Connecting to WiFi");
+  WiFiManager wm;
+  // Serial.println("Resetting settings");
+  // wm.resetSettings();
+  char ssid[32];
+  char password[64];
+  snprintf(ssid, sizeof(ssid), "Weather Station %04X",
+           (uint16_t)(ESP.getEfuseMac() & 0xFFFF));
+  snprintf(password, sizeof(password), "%012llX", ESP.getEfuseMac());
+  wm.setAPCallback([ssid, password](WiFiManager* wm) {
+    Serial.println("Launched configuration AP");
+    Serial.printf("SSID: %s\n", ssid);
+    Serial.printf("Password: %s\n", password);
+    Serial.printf("IP: %s\n", WiFi.softAPIP().toString().c_str());
+  });
+  wm.setConfigPortalTimeout(120);
+  wm.setConnectTimeout(30);
+  wm.setConfigPortalTimeoutCallback(
+      []() { Serial.println("Configuration portal timed out"); });
+  const bool res = wm.autoConnect(ssid, password);
+  if (res) {
+    Serial.println("Connected to WiFi");
+    return 0;
+  } else {
+    Serial.println("Failed to connect to WiFi");
+    return -1;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.printf("Efuse MAC: 0x%012llX\n", ESP.getEfuseMac());
+
+  connectToWiFi();
 }
 
 void loop() {
