59b51573ee921e7f6cb08e34bfbed2c5d2761d10
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-06-10 21:38:12 -0400

    Better touch funsies

--- src/main.cpp
+++ src/main.cpp
@@ -22,15 +22,33 @@ void setup() {
 }
 
 void loop() {
+  static uint32_t lastTouchMillis = 0;
+  static uint32_t lastIdleFlashMillis = 0;
+  static bool ledState = false;
+  static bool prevTouchState = false;
+
   if (isTouched()) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
-  } else {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
+    lastTouchMillis = millis();
   }
+
+  if (isTouched() && !prevTouchState) {
+    Serial.println("Touch detected");
+    prevTouchState = true;
+    digitalWrite(LED_BUILTIN, HIGH);
+    ledState = true;
+  } else if (!isTouched() && prevTouchState) {
+    Serial.println("Touch released");
+    prevTouchState = false;
+    digitalWrite(LED_BUILTIN, LOW);
+    ledState = false;
+  }
+
+  if (millis() - lastTouchMillis > 1000 &&
+      millis() - lastIdleFlashMillis > 500) {
+    digitalWrite(LED_BUILTIN, ledState ? LOW : HIGH);
+    ledState = !ledState;
+    lastIdleFlashMillis = millis();
+  }
+
+  delay(10);
 }
