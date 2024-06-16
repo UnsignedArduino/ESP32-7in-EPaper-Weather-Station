aaceca7abaf821c94c4bb495a115226d695f8065
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-06-16 17:10:27 -0400

    8g2 for text rendering

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include <Fonts/FreeMonoBold9pt7b.h>
 #include <GxEPD2_BW.h>
 #include <GxEPD2_display_selection_new_style.h>
+#include <U8g2_for_Adafruit_GFX.h>
+
+U8G2_FOR_ADAFRUIT_GFX u8g2;
 
 void setup() {
   Serial.begin(115200);
@@ -12,14 +15,24 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
 
   display.init(115200, true, 2, false);
+  u8g2.begin(display);
   display.setRotation(0);
   display.setFont(&FreeMono9pt7b);
-  display.setTextColor(GxEPD_BLACK);
   display.setFullWindow();
   display.fillScreen(GxEPD_WHITE);
 
-  display.setCursor(0, 10);
-  display.println("Hello, world!");
+  u8g2.setFontMode(1);
+  u8g2.setFontDirection(0);
+  u8g2.setBackgroundColor(GxEPD_WHITE);
+  u8g2.setForegroundColor(GxEPD_BLACK);
+
+  u8g2.setFont(u8g2_font_9x18_mf);
+  u8g2.setCursor(0, 10);
+  u8g2.println("Hello, world!");
+
+  u8g2.setCursor(0, 200);
+  u8g2.setFont(u8g2_font_wqy16_t_gb2312);
+  u8g2.println("你好，世界！");
 
   display.display(false);
   display.hibernate();
