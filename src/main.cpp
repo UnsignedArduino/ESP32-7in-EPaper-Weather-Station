cdca80670410565823d126e4bb34e0173449765e
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-06-15 22:52:53 -0400

    Hello, world!

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
+#include <Adafruit_EPD.h>
+#include <Adafruit_GFX.h>
 #include <Arduino.h>
+#include <Fonts/FreeMono9pt7b.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <GxEPD2_BW.h>
+#include <GxEPD2_display_selection_new_style.h>
 
-void setup() {}
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  display.init(115200, true, 2, false);
+  display.setRotation(0);
+  display.setFont(&FreeMono9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFullWindow();
+  display.fillScreen(GxEPD_WHITE);
+
+  display.setCursor(0, 10);
+  display.println("Hello, world!");
+
+  display.display(false);
+  display.hibernate();
+}
 
 void loop() {}
